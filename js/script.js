8c43ae0a79a460836dcddaab74d207c5a99e1826
antoniovenezia <antoniovenezia00@gmail.com>
2021-06-24 13:28:30 +0200

    vue-hello

---
+++ js/script.js
@@ -0,0 +1,15 @@
+//Descrizione: Stampare a schermo un messaggio all’interno di un h1, 
+//utilizzando i data.
+//Bonus: Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
+
+new Vue(
+    {
+        el: '#app',
+        data: {
+            text: "",
+            img: "img/img.jpeg",
+        },
+    })
+
+console.log();
